6ab9cb77c34b257b1431416937772a9971a7e680
bic-harness <bogdan.catana@harness.io>
2020-01-09 18:03:34 +0000

    Update main.tf

--- exercise_1/main.tf
+++ exercise_1/main.tf
@@ -1,7 +1,8 @@
 provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
-  region = "${var.region}"
+  region = "eu-west-2"
+  version = "2.41.0"
 }
 
 resource "aws_s3_bucket" "tf-root-module-bucket" {
