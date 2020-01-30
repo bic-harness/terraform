1cb61f5b7b2b74c4e526898d4ed7c43d728e4cc2
bic-harness <bogdan.catana@harness.io>
2020-01-30 13:57:24 +0000

    Update variables.tf

--- web_server/variables.tf
+++ web_server/variables.tf
@@ -14,11 +14,6 @@ variable "user_data_script" {
   default = <<EOF
 #!/bin/bash
 yum install httpd git curl unzip -y
-curl -O -L https://releases.hashicorp.com/terraform/0.12.20/terraform_0.12.20_linux_amd64.zip
-chmod 777 terraform_0.12.20_linux_amd64.zip
-unzip terraform_0.12.20_linux_amd64.zip
-sudo mv terraform /bin/ -f
-terraform --version
 aws s3 cp s3://bic-harness/docker_hello_world.zip /home/ec2-user/
 EOF
 }
