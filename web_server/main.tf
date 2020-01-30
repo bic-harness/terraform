259155881c4b2cf2cae7a59f4927d450e8de2432
bic-harness <bogdan.catana@harness.io>
2020-01-30 13:36:38 +0000

    Update main.tf

--- web_server/main.tf
+++ web_server/main.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "web_server" {
-  ami             = var.ami_image
-  instance_type   = "t2.micro"
-  user_data       = var.user_data_script
-  key_name        = "bc-harness"
-  security_groups = ["sg-01c4818eac2729203"]
+  ami                    = var.ami_image
+  instance_type          = "t2.micro"
+  user_data              = var.user_data_script
+  key_name               = "bc-harness"
+  vpc_security_group_ids = ["sg-01c4818eac2729203"]
   
   tags = {
-    Name        = "Web Server"
+    Name        = "WebServer"
     Environment = "Production"
   }
 }
