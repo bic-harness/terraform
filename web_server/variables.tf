6541ea3ccafc46b26dd248c1ec5d7988d7dd4f44
bic-harness <bogdan.catana@harness.io>
2020-01-31 11:50:53 +0000

    retrying scripts....

--- web_server/variables.tf
+++ web_server/variables.tf
@@ -9,3 +9,12 @@ variable "region" {
 variable "ami_image" {
   default = "ami-0089b31e09ac3fffc"
 }
+
+variable "user_data_script" {
+  default = <<EOF
+#!/bin/bash
+sudo yum install httpd git curl unzip -y
+sudo systemctl enable httpd
+sudo systemctl start httpd
+EOF
+}
