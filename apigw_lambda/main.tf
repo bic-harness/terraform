1e02813f04b960d3b10caf6f7752f843e3f946cd
bic-harness <bogdan.catana@harness.io>
2020-02-25 10:51:31 +0000

    Adjusting to better variabilise stuff

--- apigw_lambda/main.tf
+++ apigw_lambda/main.tf
@@ -13,8 +13,8 @@ resource "aws_api_gateway_resource" "rest_api_res" {
 
 # Example: request for GET /rest
 resource "aws_api_gateway_method" "request_method" {
-  rest_api_id   = var.rest_api_id
-  resource_id   = var.resource_id
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  resource_id   = aws_api_gateway_resource.rest_api_res.id
   http_method   = var.method
   authorization = "NONE"
 }
@@ -26,7 +26,7 @@ resource "aws_api_gateway_integration" "request_method_integration" {
   resource_id = aws_api_gateway_resource.rest_api_res.id
   http_method = aws_api_gateway_method.request_method.http_method
   type        = "AWS"
-  uri         = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/arn:aws:lambda:${var.region}:${var.account_id}:function:${var.lambda}/invocations"
+  uri         = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/arn:aws:lambda:${var.region}:${var.account_id}:function:${var.lambda_name}/invocations"
 
   # AWS lambdas can only be invoked with the POST method
   integration_http_method = "POST"
@@ -34,8 +34,7 @@ resource "aws_api_gateway_integration" "request_method_integration" {
 
 # lambda => GET response
 resource "aws_api_gateway_method_response" "response_method" {
-  rest_api_id = var.rest_api_id
-  //resource_id = var.resource_id
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
   resource_id = aws_api_gateway_resource.rest_api_res.id
   http_method = aws_api_gateway_integration.request_method_integration.http_method
   status_code = "200"
@@ -47,8 +46,8 @@ resource "aws_api_gateway_method_response" "response_method" {
 
 # Response for: GET /hello
 resource "aws_api_gateway_integration_response" "response_method_integration" {
-  rest_api_id = var.rest_api_id
-  resource_id = var.resource_id
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  resource_id = aws_api_gateway_resource.rest_api_res.id
   http_method = aws_api_gateway_method_response.response_method.http_method
   status_code = aws_api_gateway_method_response.response_method.status_code
 
@@ -58,11 +57,20 @@ resource "aws_api_gateway_integration_response" "response_method_integration" {
 }
 
 resource "aws_lambda_permission" "allow_api_gateway" {
-  function_name = var.lambda
+  function_name = var.lambda_name
   statement_id  = "AllowExecutionFromApiGateway"
   action        = "lambda:InvokeFunction"
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "arn:aws:execute-api:${var.region}:${var.account_id}:${var.rest_api_id}/*/${var.method}${var.path}"
+  source_arn    = "arn:aws:execute-api:${var.region}:${var.account_id}:${aws_api_gateway_rest_api.rest_api.id}/*/${aws_api_gateway_method_response.response_method.http_method}${var.path}"
+}
+
+resource "aws_api_gateway_domain_name" "apibicatananet" {
+  domain_name              = "api.bicatana.net"
+  regional_certificate_arn = "arn:aws:acm:eu-west-2:000681679761:certificate/6e87a906-5529-4fa7-b055-782c2fd1840f"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
 }
 
 resource "aws_route53_record" "api" {
@@ -72,8 +80,8 @@ resource "aws_route53_record" "api" {
   ttl     = "300"
   
   alias {
-    name                   = "${aws_elb.main.dns_name}"
-    zone_id                = "${aws_elb.main.zone_id}"
+    name                   = aws_api_gateway_domain_name.apibicatananet.regional_domain_name
+    zone_id                = aws_api_gateway_domain_name.apibicatananet.regional_zone_id
     evaluate_target_health = true
   }
 }
--- apigw_lambda/variables.tf
+++ apigw_lambda/variables.tf
@@ -2,20 +2,10 @@ variable "access_key" {}
 
 variable "secret_key" {}
 
+variable "lambda_name" {}
+
 variable "region" {
     default= "eu-west-2"
-}
-
-variable "primary_zone_id" {
-  default = "Z1PSCTOWTFWDHX"
-}
-
-variable "rest_api_id" {
-  description = "The ID of the associated REST API"
-}
-
-variable "resource_id" {
-  description = "The API resource ID"
 }
 
 variable "method" {
@@ -25,16 +15,14 @@ variable "method" {
 
 variable "path" {
   description = "The API resource path"
-}
-
-variable "lambda" {
-  description = "The lambda name to invoke"
-}
-
-variable "region" {
-  description = "The AWS region, e.g., eu-west-1"
+  default ="*"
 }
 
 variable "account_id" {
   description = "The AWS account ID"
+  default     = ""
+}
+
+variable "primary_zone_id" {
+  default = "Z1PSCTOWTFWDHX"
 }
