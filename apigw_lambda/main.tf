6293d497eda8ae95ec64d3dffdba700e5f289679
bic-harness <bogdan.catana@harness.io>
2020-02-27 10:57:10 +0000

    Update main.tf

--- apigw_lambda/main.tf
+++ apigw_lambda/main.tf
@@ -7,7 +7,7 @@ resource "aws_api_gateway_rest_api" "rest_api" {
 # The endpoint created here is: /rest
 resource "aws_api_gateway_resource" "rest_api_res" {
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
-  parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id}
+  parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
   path_part   = "rest"
 }
 
