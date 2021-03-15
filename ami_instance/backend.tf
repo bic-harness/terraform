31d103a254a214b8a5b1c9c3708ec708f1af9f9f
bic-harness <bogdan.catana@harness.io>
2021-03-15 08:07:04 +0000

    Update backend.tf

--- ami_instance/backend.tf
+++ ami_instance/backend.tf
@@ -1,7 +1,7 @@
 terraform {
    backend "s3" {
    bucket = "bic-harness"
-   key = "terraform-ami-instance.tfstate"
+   key = "terraform-ami-${var.green-alb}.tfstate"
    region = "eu-west-2"
   }
 }
