41e6e4412515ff77471eb0107cc7a47b9c85d3a4
bic-harness <bogdan.catana@harness.io>
2021-04-13 08:52:16 +0100

    Update outputs.tf

--- unscripted_ecs_cluster/outputs.tf
+++ unscripted_ecs_cluster/outputs.tf
@@ -7,46 +7,37 @@ output "region" {
 }
 
 output "vpc" {
-    value = data.selected_vpc.id
+    value = data.aws_vpc.selected_vpc.id
 }
 
 output "subnets" {
-    value = data.selected_subnets.ids
+    value = data.aws_subnet_ids.selected_subnets.ids
 }
 
 output "security_group" {
     value = var.security_groups
 }
 
-output "green_listener_name" {
-    value = aws_lb_listener.green_listener.id
+output "alb_arn" {
+    value = aws_lb.main_lb.arn
 }
 
-output "green_listener_arn" {
-    value = aws_lb_listener.green_listener.arn
+output "tg_name" {
+    value = aws_lb_target_group.main_tg.name
 }
 
-output "green_listener_arn_rule" {
-    value = aws_lb_listener_rule.green_listener_rule.arn
+output "tg_arn" {
+    value = aws_lb_target_group.main_tg.arn
 }
 
-output "green_alb" {
-//    value = var.green-alb
-    value = aws_lb.green_lb.arn
+output "listener_name" {
+    value = aws_lb_listener.main_listener.id
 }
 
-output "green_tg_1_name" {
-    value = var.green-tg-1
+output "listener_arn" {
+    value = aws_lb_listener.main_listener.arn
 }
 
-output "green_tg_1_arn" {
-    value = aws_lb_target_group.green_tg_1.arn
-}
-
-output "green_tg_2_name" {
-    value = var.green-tg-1
-}
-
-output "green_tg_2_arn" {
-    value = aws_lb_target_group.green_tg_2.arn
+output "listener_arn_rule" {
+    value = aws_lb_listener_rule.main_listener_rule.arn
 }
