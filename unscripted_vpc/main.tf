174adc651f6b519fc96eba227c9d53e4274189ec
bic-harness <bogdan.catana@harness.io>
2021-04-13 15:22:08 +0100

    Update main.tf

--- unscripted_vpc/main.tf
+++ unscripted_vpc/main.tf
@@ -11,7 +11,7 @@ module "vpc" {
 
   azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = local.isProd ? ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"] : []
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_ipv6 = true
 
