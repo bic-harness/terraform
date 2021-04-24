40911d7b9ad569931f9fd6b7512df5049b50efa9
bic-harness <bogdan.catana@harness.io>
2021-04-24 06:38:21 +0100

    changing type of ECS cluster

--- unscripted_ecs_cluster/main.tf
+++ unscripted_ecs_cluster/main.tf
@@ -2,9 +2,38 @@ locals {
  isProd = var.environment == "Prod" ? true : false 
 }
 
-
 resource "aws_ecs_cluster" "ecs-cluster" {
     name = "${var.environment}ECSCluster"
+}
+
+resource "aws_autoscaling_group" "ecs-autoscaling-group" {
+  name                        = "${var.environment}ECS-ASG"
+  availability_zones          = ["${split(",", var.availability_zones)}"]
+  max_size                    = "2"
+  min_size                    = "1"
+  desired_capacity            = "1"
+  vpc_zone_identifier         = data.aws_subnet_ids.selected_subnets.ids
+  launch_configuration        = "${aws_launch_configuration.ecs-launch-configuration.name}"
+  health_check_type           = "ELB"
+}
+
+resource "aws_launch_configuration" "ecs-launch-configuration" {
+  name                        = "${var.environment}ECS-LC"
+  image_id                    = "ami-0e4249602c03f799a"
+  instance_type               = "m5d.large"
+  iam_instance_profile        = "ECSRoleForEC2"
+  root_block_device {
+    volume_type = "standard"
+    volume_size = 30
+    delete_on_termination = true
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+  security_groups             = data.aws_security_group.selected_security_group.id
+  associate_public_ip_address = "true"
+  key_name                    = "bc-harness"
+  user_data                   = "#!/bin/bash\necho ECS_CLUSTER='${aws_ecs_cluster.ecs-cluster.name}' > /etc/ecs/ecs.config"
 }
 
 resource "aws_lb" "main_lb" {
@@ -17,7 +46,7 @@ resource "aws_lb_target_group" "main_tg" {
   name          = "${var.environment}ALB-tg"
   port          = 80
   protocol      = "HTTP"
-  target_type   = "ip"
+  target_type   = "instance"
   vpc_id        = data.aws_vpc.selected_vpc.id
   stickiness {
       type = "lb_cookie"
