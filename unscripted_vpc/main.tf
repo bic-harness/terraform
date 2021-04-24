7c1124b61576f2bd35c996df198ee07b5ca4473a
bic-harness <bogdan.catana@harness.io>
2021-04-24 06:41:25 +0100

    Update main.tf

--- unscripted_vpc/main.tf
+++ unscripted_vpc/main.tf
@@ -16,7 +16,7 @@ module "vpc" {
   enable_ipv6 = true
 
   enable_nat_gateway = true
-  single_nat_gateway = true
+  single_nat_gateway = false
 
   enable_s3_endpoint       = false
   enable_dynamodb_endpoint = false
