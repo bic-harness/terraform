1997b706aa5f5d771019a8c149d8bd24c17f0a8d
bic-harness <bogdan.catana@harness.io>
2021-04-25 06:20:23 +0100

    Update main.tf

--- unscripted_ecs_cluster/main.tf
+++ unscripted_ecs_cluster/main.tf
@@ -81,21 +81,6 @@ resource "aws_lb_listener" "main_listener" {
   }
 }
 
-resource "aws_lb_listener_rule" "main_listener_rule" {
-  listener_arn = aws_lb_listener.main_listener.arn
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.main_tg.arn
-  }
-
-  condition {
-    host_header {
-      values = ["*.*.*"]
-    }
-  }
-}
-
 resource "aws_lb_target_group_attachment" "main_attachment" {
   target_group_arn = aws_lb_target_group.main_tg.arn
   target_id        = data.aws_instance.selected_ec2_instance.id
