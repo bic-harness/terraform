cf6decb9c91cd39c77c2ee89120dfb4733461284
bic-harness <bogdan.catana@harness.io>
2021-05-07 06:25:27 +0100

    Update main.tf

--- unscripted_ecs_cluster/main.tf
+++ unscripted_ecs_cluster/main.tf
@@ -84,7 +84,6 @@ resource "aws_lb_listener" "main_listener" {
 }
 
 resource "aws_lb_target_group_attachment" "main_attachment" {
-  count            = local.isProd ? 0 : 1
   target_group_arn = aws_lb_target_group.main_tg.arn
   target_id        = data.aws_instance.selected_ec2_instance.id
   port             = 80
@@ -119,7 +118,7 @@ resource "aws_route53_record" "staging" {
 resource "aws_route53_record" "prod" {
   count   = local.isProd ? 1 : 0
   zone_id = var.primary_zone_id
-  name    = "prod.bicatana.net"
+  name    = "unscripted.bicatana.net"
   type    = "A"
 
   alias {
